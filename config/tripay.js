9d0a319530eb486eb0913c45c737b71e80fad174
ArgasID <store.argasid@gmail.com>
2025-05-24 11:28:35 +0700

    Update

--- config/tripay.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    kodeMerchant: 'T40499',
-    apiKey: 'PCYJ6jKIFZgmMlF26cm5SDLBmbeR678VuBzrZqIF',
-    privateKey: 'WN7qd-YWXNB-B3Z43-Je36m-uKTGG',
-    urlBuatTransaksi: 'https://tripay.co.id/api/transaction/create',
-    callbackUrl: 'https://web.glowbit.fun/callback',
-    returnUrl: 'https://web.glowbit.fun/redirect'
-};
